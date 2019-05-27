3e052b189beed2629e4151f42d12f172a8085d42
RaduAlbastroiu <albastroiuradug@gmail.com>
2019-05-27 17:11:40 +0300

    Update Web Cam Object Detection

--- ImageClasification/WebCamObjectDetection.py
+++ ImageClasification/WebCamObjectDetection.py
@@ -7,6 +7,28 @@ import tensorflow as tf
 import zipfile
 
 from collections import defaultdict
-from io import StringID
+from io import StringIO
 from matplotlib import pyplot as plt
 from PIL import Image
+
+from object_detection.utils import ops as utils_ops
+#from utils import label_map_util
+#from utils import visualization_utils as vis_util
+
+import cv2
+cap = cv2.VideoCapture(0)
+
+MODEL_NAME = 'ssd_mobilenet_v1_coco_2017_11_17'
+MODEL_FILE = MODEL_NAME + '.tar.gz'
+DOWNLOAD_BASE = 'http://download.tensorflow.org/models/object_detection/'
+
+PATH_TO_FROZEN_GRAPH = MODEL_NAME + '/frozen_inference_graph.pb'
+PATH_TO_LABELS = os.path.join('data', 'mscoco_label_map.pbtxt')
+
+NUM_CLASSES = 90
+
+
+# Download model
+
+
+# Load a frozen model
