60dcc1dc3efab8b8a1230fbb40db4bb67960c77e
RaduAlbastroiu <albastroiuradug@gmail.com>
2019-05-28 13:14:18 +0300

    Added Models-master to be able to run

--- ImageClasification/WebCamObjectDetection.py
+++ ImageClasification/WebCamObjectDetection.py
@@ -12,8 +12,8 @@ from matplotlib import pyplot as plt
 from PIL import Image
 
 from object_detection.utils import ops as utils_ops
-from utils import label_map_util
-from utils import visualization_utils as vis_util
+from object_detection.utils import label_map_util
+from object_detection.utils import visualization_utils as vis_util
 
 import cv2
 cap = cv2.VideoCapture(0)
