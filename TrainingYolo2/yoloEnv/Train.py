5ff4a11a7e6b34ca0225b2d98367086731f6b2ac
RaduAlbastroiu <albastroiuradug@gmail.com>
2019-06-02 23:57:42 +0300

    Modified Train num of epochs

--- TrainingYolo2/yoloEnv/Train.py
+++ TrainingYolo2/yoloEnv/Train.py
@@ -8,11 +8,11 @@ config.gpu_options.allow_growth = False
 with tf.Session(config=config) as sess:
   options = {'model':'cfg/yolo-train.cfg',
           'load':'yolov2-voc.weights', 
-          'epoch':100,
+          'epoch':15000,
           'train':True,
           'annotation': '/Users/radualbastroiu/Documents/My_projects/Licenta/ML-Object-Recognition/data/partial_soccer_ball_data/annotations/',
-          'dataset': '/Users/radualbastroiu/Documents/My_projects/Licenta/ML-Object-Recognition/data/partial_soccer_ball_data/images/'
-          #'gpu':1.0
+          'dataset': '/Users/radualbastroiu/Documents/My_projects/Licenta/ML-Object-Recognition/data/partial_soccer_ball_data/images/',
+          'gpu':1.0
           }
   tfnet = TFNet(options)
 
